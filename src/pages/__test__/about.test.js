4e3309c3266ac287511b6069081b7760641ff884
Theresa Marquis <tmarquis@kalmbach.com>
2018-10-22 14:59:18 -0500

    Add three snapshot tests

---
+++ src/pages/__test__/about.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import About from '../about';
+
+describe('About', ()=>{
+	it('renders correctly', ()=>{
+		const tree = renderer.create(<About />).toJSON()
+		expect(tree).toMatchSnapshot()
+	})
+})
