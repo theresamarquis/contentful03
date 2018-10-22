bb77e83bdbe0d26094eebb7813480e2ea05810df
Theresa Marquis <tmarquis@kalmbach.com>
2018-10-22 11:48:55 -0500

    Remove airbnb lint

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,36 +2,36 @@ import React, { Component } from 'react';
 import { Link } from 'gatsby';
 
 class ContactForm extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            value: ''
-        }
-    }
+	constructor(props){
+		super(props)
+		this.state = {
+			value: ''
+		}
+	}
 
-    handleSubmit = (event)=>{
-        event.preventDefault();
-        alert('A name was submitted: ' + this.state.value);
+    handleSubmit = (event) => {
+    	event.preventDefault();
+    	alert('A name was submitted: ' + this.state.value);
     }
 
     changeHandler=(event)=>{
-        this.setState({value: event.target.value})
-    }
+    	this.setState({value: event.target.value})
+    };
 
     render() {
-        return(
-            <div>
-                <Link to="/">Home</Link>
-            <form onSubmit={this.submitHandler}>
-                <label>
+    	return(
+    		<div>
+    			<Link to="/">Home</Link>
+    			<form onSubmit={this.submitHandler}>
+    				<label>
                     Email:
-                    <input placeholder="email" onChange={this.changeHandler}></input>
-                </label>
-                <input type='submit' value='Submit' ></input>
-            </form>
-            </div>
+    					<input placeholder="email" onChange={this.changeHandler}></input>
+    				</label>
+    				<input type='submit' value='Submit' ></input>
+    			</form>
+    		</div>
             
-        )
+    	)
     }
 }
 
